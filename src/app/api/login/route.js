62bb931826356bf3d61f7ed1dfd2bc22915b9256
stedHamzaArif <shamzaarif1998@gmail.com>
2025-03-16 14:20:12 +0100

    bugs fixed

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -1,6 +1,7 @@
 import { prisma } from "@/utils/db";
 import bcrypt from "bcrypt";
 import { response } from "@/utils/response";
+import jwt from "jsonwebtoken";
 
 export async function POST(request) {
   try {
@@ -18,7 +19,11 @@ export async function POST(request) {
       return response.unauthorized("Invalid credentials.");
     }
 
-    return response.success("Login successful!", { email: user.email });
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    return response.success("Login successful!", { email: user.email, token });
   } catch (error) {
     return response.serverError("Registration failed. Please try again.");
   }
